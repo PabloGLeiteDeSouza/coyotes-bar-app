b1af75ec3f27cd0b205d71cccabce2d71fc68a3b
PabloGLeiteDeSouza <pablo.guilherme.leite.de.souza@gmail.com>
2024-06-11 18:08:12 -0300

    "Wrapped select component with Box and rearranged elements in Create screen"

--- screens/vendas/create.tsx
+++ screens/vendas/create.tsx
@@ -120,77 +120,80 @@ export const Create: React.FC = () => {
               </FormControlErrorText>
             </FormControlError>
           </FormControl>
-
-          {Array.from({
-            length: quantidade_de_produtos,
-          }).map((_, index) => (
-            <>
-              {/* Produto */}
-              <FormControl
-                isInvalid={false}
-                size={"md"}
-                isDisabled={false}
-                isRequired={true}
-              >
-                <FormControlLabel>
-                  <FormControlLabelText>Produto {index+1}</FormControlLabelText>
-                </FormControlLabel>
-                    <Select
-                        variant={"outline"}
-                        isInvalid={true}
+            <Box
+                mt="$5"
+                gap="$5"
+            >
+                {Array.from({
+                    length: quantidade_de_produtos,
+                }).map((_, index) => (
+                    <>
+                    {/* Produto */}
+                    <FormControl
+                        isInvalid={false}
+                        size={"md"}
                         isDisabled={false}
+                        isRequired={true}
                     >
-                    <SelectTrigger size={"lg"} variant={"outline"}>
-                        <SelectInput placeholder="Select option" />
-                        <SelectIcon
-                        mr={"$3"}
-                        ml={0}
-                        as={ChevronDownIcon}
-                        />
-                    </SelectTrigger>
-                    <SelectPortal>
-                        <SelectBackdrop />
-                        <SelectContent>
-                        <SelectDragIndicatorWrapper>
-                            <SelectDragIndicator />
-                        </SelectDragIndicatorWrapper>
-                        <SelectItem label="UX Research" value="UX Research" />
-                        <SelectItem
-                            label="Web Development"
-                            value="Web Development"
-                        />
-                        <SelectItem
-                            label="Cross Platform Development Process"
-                            value="Cross Platform Development Process"
-                        />
-                        <SelectItem
-                            label="UI Designing"
-                            value="UI Designing"
-                            isDisabled={true}
-                        />
-                        <SelectItem
-                            label="Backend Development"
-                            value="Backend Development"
-                        />
-                        </SelectContent>
-                    </SelectPortal>
-                    </Select>
+                        <FormControlLabel>
+                        <FormControlLabelText>Produto {index+1}</FormControlLabelText>
+                        </FormControlLabel>
+                            <Select
+                                isInvalid={true}
+                                isDisabled={false}
+                            >
+                            <SelectTrigger size={"lg"} variant={"outline"}>
+                                <SelectInput placeholder="Select option" />
+                                <SelectIcon
+                                mr={"$3"}
+                                ml={0}
+                                as={ChevronDownIcon}
+                                />
+                            </SelectTrigger>
+                            <SelectPortal>
+                                <SelectBackdrop />
+                                <SelectContent>
+                                <SelectDragIndicatorWrapper>
+                                    <SelectDragIndicator />
+                                </SelectDragIndicatorWrapper>
+                                <SelectItem label="UX Research" value="UX Research" />
+                                <SelectItem
+                                    label="Web Development"
+                                    value="Web Development"
+                                />
+                                <SelectItem
+                                    label="Cross Platform Development Process"
+                                    value="Cross Platform Development Process"
+                                />
+                                <SelectItem
+                                    label="UI Designing"
+                                    value="UI Designing"
+                                    isDisabled={true}
+                                />
+                                <SelectItem
+                                    label="Backend Development"
+                                    value="Backend Development"
+                                />
+                                </SelectContent>
+                            </SelectPortal>
+                            </Select>
 
-                <FormControlHelper>
-                  <FormControlHelperText>
-                    Must be atleast 6 characters.
-                  </FormControlHelperText>
-                </FormControlHelper>
+                        <FormControlHelper>
+                        <FormControlHelperText>
+                            Must be atleast 6 characters.
+                        </FormControlHelperText>
+                        </FormControlHelper>
 
-                <FormControlError>
-                  <FormControlErrorIcon as={AlertCircleIcon} />
-                  <FormControlErrorText>
-                    Atleast 6 characters are required.
-                  </FormControlErrorText>
-                </FormControlError>
-              </FormControl>
-            </>
-          ))}
+                        <FormControlError>
+                        <FormControlErrorIcon as={AlertCircleIcon} />
+                        <FormControlErrorText>
+                            Atleast 6 characters are required.
+                        </FormControlErrorText>
+                        </FormControlError>
+                    </FormControl>
+                    </>
+                ))}
+            </Box>
         </Box>
       </Box>
     </ScrollView>
